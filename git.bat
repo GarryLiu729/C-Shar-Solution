714840c004583226c400927f4ca2c871d456e63c
GarryLiu729 <165274593+GarryLiu729@users.noreply.github.com>
2025-12-11 00:30:31 -0600

    update his

---
+++ git.bat
@@ -0,0 +1,64 @@
+git filter-branch -f --env-filter '
+    if test "$GIT_AUTHOR_EMAIL" = "112554171+dylanprovost@users.noreply.github.com"
+    then
+        GIT_AUTHOR_NAME="garryliu729"
+        GIT_AUTHOR_EMAIL="james.net.wang@gmail.com"
+    fi
+    if test "$GIT_COMMITTER_EMAIL" = "49699333+dependabot[bot]@users.noreply.github.com"
+    then
+        GIT_COMMITTER_NAME="garryliu729"
+        GIT_COMMITTER_EMAIL="james.net.wang@gmail.com"
+    fi
+    if test "$GIT_COMMITTER_EMAIL" = "43530141+IvanMaidurov@users.noreply.github.com"
+    then
+        GIT_COMMITTER_NAME="garryliu729"
+        GIT_COMMITTER_EMAIL="james.net.wang@gmail.com"
+    fi
+    if test "$GIT_COMMITTER_EMAIL" = "kirill.grigorev@omnevo.net"
+    then
+        GIT_COMMITTER_NAME="garryliu729"
+        GIT_COMMITTER_EMAIL="james.net.wang@gmail.com"
+    fi
+    if test "$GIT_COMMITTER_EMAIL" = "3203968+carstendietrich@users.noreply.github.com"
+    then
+        GIT_COMMITTER_NAME="garryliu729"
+        GIT_COMMITTER_EMAIL="james.net.wang@gmail.com"
+    fi
+    if test "$GIT_COMMITTER_EMAIL" = "k.nowak021@gmail.com"
+    then
+        GIT_COMMITTER_NAME="garryliu729"
+        GIT_COMMITTER_EMAIL="james.net.wang@gmail.com"
+    fi
+    if test "$GIT_COMMITTER_EMAIL" = "carsten.dietrich@omnevo.net"
+    then
+        GIT_COMMITTER_NAME="garryliu729"
+        GIT_COMMITTER_EMAIL="james.net.wang@gmail.com"
+    fi
+    if test "$GIT_COMMITTER_EMAIL" = "stanislav@lifedraft.de"
+    then
+        GIT_COMMITTER_NAME="garryliu729"
+        GIT_COMMITTER_EMAIL="james.net.wang@gmail.com"
+    fi
+    if test "$GIT_COMMITTER_EMAIL" = "tessig@users.noreply.github.com"
+    then
+        GIT_COMMITTER_NAME="garryliu729"
+        GIT_COMMITTER_EMAIL="james.net.wang@gmail.com"
+    fi
+        if test "$GIT_COMMITTER_EMAIL" = "christopher.schwab@omnevo.net"
+    then
+        GIT_COMMITTER_NAME="garryliu729"
+        GIT_COMMITTER_EMAIL="james.net.wang@gmail.com"
+    fi
+        if test "$GIT_COMMITTER_EMAIL" = "tessig@users.noreply.github.com"
+    then
+        GIT_COMMITTER_NAME="garryliu729"
+        GIT_COMMITTER_EMAIL="james.net.wang@gmail.com"
+    fi
+        if test "$GIT_COMMITTER_EMAIL" = "tessig@users.noreply.github.com"
+    then
+        GIT_COMMITTER_NAME="garryliu729"
+        GIT_COMMITTER_EMAIL="james.net.wang@gmail.com"
+    fi
+' HEAD
+
+k.nowak021@gmail.com
